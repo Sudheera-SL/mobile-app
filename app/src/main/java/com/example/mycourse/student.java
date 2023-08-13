665c00b78349473eeadb84c919f08908053bb9f8
sudheera <subashni21426@gmail.com>
2023-08-13 13:03:57 +0530

    1st commit

---
+++ app/src/main/java/com/example/mycourse/student.java
@@ -0,0 +1,11 @@
+package com.example.mycourse;
+
+    public class student {
+
+        String id;
+        String name;
+        String course;
+        String fee;
+        String titles;
+
+    }
